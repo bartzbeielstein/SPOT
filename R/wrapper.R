b1377863a42bf5102f72e6f8ea33c40c66cbcff4
Frederik Rehbach <frederik.re@gmail.com>
2019-06-20 13:26:18 +0200

    Add a pararallelizable function wrapper

--- R/wrapper.R
+++ R/wrapper.R
@@ -33,3 +33,36 @@ wrapFunction <- function(fun){
   return(function(x){matrix(apply(x,1,fun),,1)})
 }
 
+#' Parallelized Function Evaluation Wrapper
+#'
+#' This is a simple wrapper that turns a function of type \code{y=f(x)}, where x is a vector and y is a scalar,
+#' into a function that accepts and returns matrices, as required by \code{\link{spot}}.
+#' While doing so, the wrapper will use the parallel package in order to parallelize the execution of each function
+#' evaluation. This function will create a computation cluster if no cluster is specified and there is no
+#' default cluster setup!
+#'
+#' @param fun the function that shall be evaluated in parallel
+#' @param cl Optional, an existing computation cluster
+#' @param nCores Optional, amount of cores to use for creating a new computation cluster. Default is all cores.
+#'
+#' @return numeric vector, result of the parallelized evaluation
+#' @export
+#'
+#' @examples
+parallelWrapFunction <- function(fun, cl = NULL, nCores = NULL){
+    if(is.null(cl)){
+        cl <- parallel::getDefaultCluster()
+        if(is.null(cl)){
+            if(is.null(nCores)){
+                nCores <- parallel::detectCores()
+            }
+            parallel::setDefaultCluster(parallel::makeCluster(nCores))
+        }
+    }
+    return(
+        function(x){
+            res <- parallel::parApply(cl,x,1,fun)
+            matrix(res,,1)
+        }
+    )
+}
